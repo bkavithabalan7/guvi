3047b989abdc87489b9b2e28150ff80747b0bd74
BKavitha <balanbkavitha18@outlook.com>
2016-06-25 11:02:59 +0530

    Create vowelorcons.java

---
+++ vowelorcons.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+public class Vowel{
+public static void main(String[] args){
+Scanner read= new Scanner(System.in);
+System.out.println("Enter a Alphabet:");
+char c = read.nextChar();
+if(c=='A'||c=='a'||c=='E'||c=='e'||c=='I'||c=='i'||c=='O'||c=='o'||c=='U'||c=='u'){
+System.out.println("VOWEL");}
+else
+System.out.println("CONSONANT");
+}}
