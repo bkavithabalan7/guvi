df4e7bf6bae344ebac372804bb55e02e1d715ee9
BKavitha <balanbkavitha18@outlook.com>
2016-06-25 11:50:52 +0530

    Update largest.java

--- largest.java
+++ largest.java
@@ -8,11 +8,11 @@ x = in.nextInt();
 y = in.nextInt();
 z = in.nextInt();
  if(x>y && x>z)
-     System.out.println(x +" is largest.");
+     System.out.println(x +" is the largest number.");
  else if(y>x && y>z)
-    System.out.println(y +" is largest.");
+    System.out.println(y +" is the largest number.");
  else if (z>x && z>y)
-   System.out.println(z +" is largest.");
+   System.out.println(z +" is the largest number.");
  else   
    System.out.println("Entered numbers are not distinct.");
    }}
