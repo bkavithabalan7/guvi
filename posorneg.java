97d88001cd31e1d1f2366420f92568ec6e28e448
BKavitha <balanbkavitha18@outlook.com>
2016-06-25 10:02:20 +0530

    Update posorneg.java

--- posorneg.java
+++ posorneg.java
@@ -1,5 +1,5 @@
 import java.util.scanner;
-public class pos{
+public class Pos{
 public static void main(String[] args) { 
 Scanner input = new Scanner(System.in);
 System.out.print("Enter a number: ");
