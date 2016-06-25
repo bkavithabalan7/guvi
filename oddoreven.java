12278167a5eb138c8a327fbb7fc45c3afdffaae8
BKavitha <balanbkavitha18@outlook.com>
2016-06-25 10:21:47 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+public class Check{
+public static void main(String[] args) { 
+Scanner input = new Scanner(System.in);
+System.out.println("Enter a number: ");
+int num=input.nextInt();
+if((num%2)==0){
+System.out.println("EVEN");}
+else
+System.out.println("ODD");
+}}
+
