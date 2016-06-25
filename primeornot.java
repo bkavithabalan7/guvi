b50dd247d34df62c7b85157743e52720c516c686
BKavitha <balanbkavitha18@outlook.com>
2016-06-25 12:44:42 +0530

    Create primeornot.java

---
+++ primeornot.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PrimeCheck{
+public static void main(String args[]){
+	int temp;
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter a number for check:");
+int num=scan.nextInt();
+for(int i=2;i<=num/2;i++){
+           if((temp=num%i)==0){
+	   System.out.println("The Given string"+num +"is NOT A PRIME");}
+	   System.out.println("The Given String "+ num +"is PRIME");
+}}}
+	
