11384329abe5b0b2a3a523f2b8eaac593328dafa
BKavitha <balanbkavitha18@outlook.com>
2016-06-27 09:34:02 +0530

    Update primeornot.java

--- primeornot.java
+++ primeornot.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 	Scanner scan= new Scanner(System.in);
 	System.out.println("Enter a number for check:");
 int num=scan.nextInt();
-for(int i=2;i<=num/2;i++){
+for(int i=2;i<=num/2;++i){
            if((temp=num%i)==0){
 	   System.out.println("The Given string"+num +"is NOT A PRIME");}
 	   System.out.println("The Given String "+ num +"is PRIME");
